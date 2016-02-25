fecaee8f1af5304e3c5cc8d4da038255a486a002
Arjan Molenaar <gaphor@gmail.com>
2016-02-25 10:49:39 +0100

    Make image definition more customizable.

--- test/conftest.py
+++ test/conftest.py
@@ -1,16 +1,32 @@
-import sys
 import pytest
 import testinfra
 
-# docker run -i -t -v /srv/salt/:/srv/salt -v /srv/pillar/:/srv/pillar/ -v /srv/files/:/srv/files/ salt-test-base /bin/bash
 
 target_boxes = ['centos7-salt', 'ubuntu15-salt']
 
+
+def root_dir():
+    """
+    Figure out the project base dir (parent folder of file)
+    """
+    from os.path import dirname
+    return dirname(dirname(__file__))
+
+
 @pytest.fixture(scope='module', params=target_boxes)
-def docker_image(request, LocalCommand):
-    cmd = LocalCommand("docker build -t %s %s", request.param, request.param)
-    assert cmd.rc == 0
+def image_name(request):
+    """
+    This fixture returns the image names to test against.
+    Override this fixture in your module if you need to test with different images.
+    """
     return request.param
+
+
+@pytest.fixture(scope='module')
+def docker_image(image_name, LocalCommand):
+    cmd = LocalCommand("docker build -t %s %s", image_name, image_name)
+    assert cmd.rc == 0
+    return image_name
     
 
 @pytest.fixture(scope='module')
@@ -18,8 +34,11 @@ def Docker(request, docker_image, LocalCommand):
     """
     Boot and stop a docker image. The image is primed with salt-minion.
     """
+    base = root_dir()
+    print 'Project base dir is:', base
+
     # Run a new container. Run in privileged mode, so systemd will start
-    docker_id = LocalCommand.check_output("docker run --privileged -d -v ../salt/:/srv/salt -v ../pillar/:/srv/pillar/ %s", docker_image)
+    docker_id = LocalCommand.check_output("docker run --privileged -d -v %s/salt/:/srv/salt -v %s/pillar/:/srv/pillar/ %s", base, base, docker_image)
 
     def teardown():
         LocalCommand.check_output("docker kill %s", docker_id)
@@ -38,8 +57,7 @@ def docker_backend_provision_as(self, minion_id):
     Command = self.get_module("Command")
     print 'Executing salt-call locally for id', minion_id
     cmd = Command("salt-call --local --force-color --retcode-passthrough --id=%s.testenv state.highstate", minion_id)
-    sys.stdout.write(cmd.stdout)
-    #sys.stderr.write(cmd.stderr)
+    print cmd.stdout
     assert cmd.rc == 0
     return cmd
 
